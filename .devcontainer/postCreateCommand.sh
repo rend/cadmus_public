f92ebd5ed6d9afcae7b1f15fa5572a618e96b0ea
rend <matthews.will@gmail.com>
2023-11-29 22:46:13 +0000

    Sort keys out

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #! usr/bin/bash
 sudo apt-get update -y
 sudo pip install pip-tools
-sudo pip-compile /workspaces/cadmus/requirements.in
-sudo pip-sync /workspaces/cadmus/requirements.txt
+sudo pip-compile /workspaces/cadmus_public/requirements.in
+sudo pip-sync /workspaces/cadmus_public/requirements.txt
